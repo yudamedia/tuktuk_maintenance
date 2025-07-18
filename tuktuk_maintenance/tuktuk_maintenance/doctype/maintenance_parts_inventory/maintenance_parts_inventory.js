abe508f10414e7f4157c612adfb91ea11d0b7dc9
Yuda Media <yudamedia@gmail.com>
2025-07-18 16:16:30 +0000

    fix: Update maintenance parts inventory JSON to correct field precision types, enhance category options, and adjust creation and modified timestamps for consistency.

---
+++ tuktuk_maintenance/tuktuk_maintenance/doctype/maintenance_parts_inventory/maintenance_parts_inventory.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Yuda Media and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Maintenance Parts Inventory", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tuktuk_maintenance/tuktuk_maintenance/doctype/maintenance_parts_inventory/test_maintenance_parts_inventory.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Yuda Media and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMaintenancePartsInventory(FrappeTestCase):
+	pass
